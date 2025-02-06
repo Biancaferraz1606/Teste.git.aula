412243812a7de1141617808b2740e3d46cbb2941
bianca <bianca.sferraz1@senacsp.edu.br>
2025-02-06 19:22:19 -0300

    alterando

--- index/index.js
+++ index/index.js
@@ -1,1 +1,12 @@
-dispatchEvent=this
+const celular = {
+    tamanho: 5,
+    cor: "azul",
+    sistema: "ios",
+    qtdeCameras: 3,
+    processador: "a12",
+    qtdeMemoriaaRam: 6,
+    qtdeMemoriaInternet: 128,
+    tocar: function(){
+        console.log("trim trim trim")
+    }
+}
